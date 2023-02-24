a400aaa2f4c35e38cdd050909a97403436b12b75
mekrograca <grkem.squash99@gmail.com>
2023-02-24 14:57:09 +0300

    dynamic error view

---
+++ error.vue
@@ -0,0 +1,15 @@
+<script setup>
+const error = useError();
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen justify-center items-center">
+        <h1 class="md:text-9xl font-bold ">{{ error.statusCode }}</h1>
+        <p class="text-sm">
+            {{ error.message }}
+        </p>
+        <nuxt-link to="/" class="mt-8 font-bold bg-slate-200 px-4 py-2 rounded-lg">
+            Go Back
+        </nuxt-link>
+    </div>
+</template>
