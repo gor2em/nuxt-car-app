88e3cfd2deb6130a755aafd7704ef1fa31447e85
mekrograca <grkem.squash99@gmail.com>
2023-02-25 23:18:29 +0300

    added car make filter

--- components/Car/SideBar.vue
+++ components/Car/SideBar.vue
@@ -1,5 +1,7 @@
 <script setup>
 
+const { makes } = useCars();
+
 const modal = ref({
     modal: false,
     location: false,
@@ -28,10 +30,16 @@ const onChangeLocation = () => {
     navigateTo(`/city/${city.value}/car/${route.params.make}`)
     city.value = "";
 }
+
+const onChangeMake = (make) => {
+    updateModal("make");
+    navigateTo(`/city/${route.params.city}/car/${make}`)
+}
 </script>
 
 <template>
     <div class="shadow border w-64 mr-10 z-30 h-[190px]">
+        <!--LOCATION START-->
         <div class="p-5 flex justify-between relative cursor-pointer border-b last:border-none gap-8">
             <h3>Location</h3>
             <h3 @click="updateModal('location')" class="text-blue-400 capitalize">{{ route.params.city }}</h3>
@@ -42,5 +50,27 @@ const onChangeLocation = () => {
                 </button>
             </div>
         </div>
+        <!--LOCATION END-->
+
+        <!--MAKE START-->
+        <div class="p-5 flex justfiy-between relative cursor-pointer border-b gap-8">
+            <h3>Make</h3>
+            <h3 class="text-blue-400 capitalize" @click="updateModal('make')">{{ route.params.make || "Any" }}</h3>
+
+            <div class="absolute border shadow left-60 p-5 top-1 -m-1 w-[600px] flex justify-between flex-wrap bg-white"
+                v-if="modal.make">
+                <h4 v-for="make in makes" :key="make" class="w-1/3" @click="onChangeMake(make)">
+                    {{ make }}
+                </h4>
+            </div>
+
+        </div>
+        <!--MAKE END-->
+
+        <div class="p-5 flex justfiy-between relative cursor-pointer">
+            <h3>Price</h3>
+            <h3 class="text-blue-400 capitalize"></h3>
+        </div>
+
     </div>
 </template>
