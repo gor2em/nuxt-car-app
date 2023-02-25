9d726c1c3b656d2c42ab5a5608818932cad66f4b
mekrograca <grkem.squash99@gmail.com>
2023-02-25 22:55:14 +0300

    vueUse and emit

--- components/Car/Card.vue
+++ components/Car/Card.vue
@@ -4,20 +4,22 @@ import heartFilled from "@/assets/heartFilled.png";
 import heartOutline from "@/assets/heartOutline.png";
 
 const props = defineProps({
-    car: Object
+    car: Object,
+    favored: Boolean,
 });
 
 //unique
-const favored = useState(`favored-${props.car.id}`, () => {
-    return false
-})
+// const favored = useState(`favored-${props.car.id}`, () => {
+//     return false
+// })
 
+const emit = defineEmits(['favor'])
 </script>
 
 <template>
     <div class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]">
         <img :src="favored ? heartFilled : heartOutline" class="absolute w-7 right-5 top-2 z-20"
-            @click="favored = !favored">
+            @click="emit('favor', car.id)">
         <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
             <NuxtImg :src="car.url" class="w-[300px] h-full" alt="" />
             <div class="p-4 flex flex-col">
